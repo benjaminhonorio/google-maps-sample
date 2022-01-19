751c94baf35fa3c24f1f9ace9604fee9d3501c64
benjaminhonorio <benjaminhonorio.12@gmail.com>
2022-01-19 15:50:41 -0500

    Move from typescript to javascript from sample app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import { Wrapper } from "@googlemaps/react-wrapper";
+import Map from "./Components/Map";
+import Marker from "./Components/Marker";
+import { useState } from "react";
 
 function App() {
+  // Shows Loading or loads the map if it's ready
+  const render = (status) => {
+    return <h1>{status}</h1>;
+  };
+  const [clicks, setClicks] = useState([]);
+  const [zoom, setZoom] = useState(3); // initial zoom
+  const [center, setCenter] = useState({ lat: 0, lng: 0 });
+
+  const onClick = (e) => {
+    setClicks([...clicks, e.latLng]);
+  };
+
+  const onIdle = (m) => {
+    console.log("onIdle");
+    setZoom(m.getZoom());
+    setCenter(m.getCenter().toJSON());
+  };
+
+  const form = (
+    <div
+      style={{
+        padding: "1rem",
+        flexBasis: "250px",
+        height: "100%",
+        overflow: "auto",
+      }}
+    >
+      <label htmlFor="zoom">Zoom</label>
+      <input
+        type="number"
+        id="zoom"
+        name="zoom"
+        value={zoom}
+        onChange={(event) => setZoom(Number(event.target.value))}
+      />
+      <br />
+      <label htmlFor="lat">Latitude</label>
+      <input
+        type="number"
+        id="lat"
+        name="lat"
+        value={center.lat}
+        onChange={(event) =>
+          setCenter({ ...center, lat: Number(event.target.value) })
+        }
+      />
+      <br />
+      <label htmlFor="lng">Longitude</label>
+      <input
+        type="number"
+        id="lng"
+        name="lng"
+        value={center.lng}
+        onChange={(event) =>
+          setCenter({ ...center, lng: Number(event.target.value) })
+        }
+      />
+      <h3>{clicks.length === 0 ? "Click on map to add markers" : "Clicks"}</h3>
+      {clicks.map((latLng, i) => (
+        <pre key={i}>{JSON.stringify(latLng.toJSON(), null, 2)}</pre>
+      ))}
+      <button onClick={() => setClicks([])}>Clear</button>
+    </div>
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div style={{ display: "flex", height: "100%" }}>
+      <Wrapper apiKey={process.env.REACT_APP_MAPS_API_KEY} render={render}>
+        <Map
+          center={center}
+          onClick={onClick}
+          onIdle={onIdle}
+          zoom={zoom}
+          style={{ flexGrow: "1", height: "100vh" }}
         >
-          Learn React
-        </a>
-      </header>
+          {clicks.map((latLng, i) => (
+            <Marker key={i} position={latLng} />
+          ))}
+        </Map>
+      </Wrapper>
+      {/* Basic form for controlling center and zoom of map. */}
+      {form}
     </div>
   );
 }
-
 export default App;
